ce7cb64e07deb1c5105941187c91063512dc66c9
yuliagitaps77 <158534580+yuliagitaps77@users.noreply.github.com>
2024-09-26 10:19:45 +0700

    Add files via upload

---
+++ acara11web/case1.php
@@ -0,0 +1,14 @@
+<?php
+interface Bird {
+    public function makeSound();
+}
+
+class Perkutut implements Bird {
+    public function makeSound() {
+        echo "Kur Kur";
+    }
+}
+
+$burung = new Perkutut();
+$burung->makeSound();
+?>
